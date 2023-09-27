6cd74cae45bbd4842b9b1eed5ef825ecced25756
ASdashM <putyatindanil30@gmail.com>
2023-09-27 18:43:42 +0300

    final

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
 
-function App() {
+const App = () => {
+
+  const [tasks, setTasks] = useState([
+    {id: 1, title: 'Learn React', completed: false},
+    {id: 2, title: 'Learn React', completed: false},
+    {id: 3, title: 'Learn React', completed: false},
+  ])
+
+  const[input, setInput] = useState('')
+
+  function todoComplete(id) {
+    setTasks(tasks.filter(task => {
+      if(task.id === id) {
+        task.completed = !task.completed
+      }
+      return task
+    }))
+  }
+
+  function removeTask(id) {
+    setTasks(tasks.filter(task => task.id !== id))
+  }
+
+  function addTask (e) {
+      if (e.code === "Enter" && input.trim() !== '' ) {
+        setTasks(tasks.concat([{id: Date.now(), title: input}]))
+        setInput('')
+      }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>ToDo App</h1>
+      <input value={input} type="text" onKeyUpCapture={(e)=> addTask(e)} onChange={(e)=> setInput(e.target.value)}/>
+      {tasks && tasks.map(task => {
+        return (
+          <div>
+            <div className='taskWrapper' key={task.id}>
+              <input type="checkbox" onClick={()=> todoComplete(task.id)}/>
+              <div className="taskTitle" style={{textDecoration: task.completed?"line-through": null}}>{task.title}</div>
+              <div onClick={()=> removeTask(task.id)} className='close'>&times;</div>
+            </div>
+          </div>
+        )
+      })}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
